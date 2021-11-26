c2d9b8717bb16a9643ca05584aee04154789e433
Zhen3r <xzzxzz2010@163.com>
2021-11-25 22:12:07 -0500

    change p2

--- web/static/main.js
+++ web/static/main.js
@@ -1,27 +1,50 @@
 
 // define basemap
-let basemap = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+let basemap01 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 })
 
+let basemap02 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+
+getQuantileBreaks = function (geojson, propertyName, breakNum) {
+    let classData = [];
+    geojson.features.forEach(feature => {
+        classData.push(feature.properties[propertyName]);
+    });
+    return chroma.limits(classData, "q", breakNum);
+}
 
 // map01
-var map01 = L.map('map01').setView([39.9897471840457, -75.13893127441406], 11)
-basemap.addTo(map01);
+var map01 = L.map('map01').setView([39.9597471840457, -75.17893127441406], 14)
+basemap02.addTo(map01);
 // Add roads
 $.getJSON('data/roads.geojson', function (geojson) {
+    let quantileBreaks = getQuantileBreaks(geojson, "CLASS", 6);
     L.geoJSON(geojson, {
         style: (feature) => {
-            let w = 1 - feature.properties["CLASS"] / 5;
-
-            return { weight: w, color: "black" };
+            let classNum = feature.properties["CLASS"];
+            // let c = chroma.scale().classes(quantileBreaks)(feature.properties["CLASS"])
+            let o = (classNum <= 3 ? 1 : .5)
+            return {
+                weight: 0.5,
+                color: "#000",
+                opacity: o,
+            };
         }
-    }).addTo(map01)
+    }).bringToBack().addTo(map01)
 })
 // Add street view points
 $.getJSON('data/streetViewPoints.geojson', function (geojson) {
     L.geoJSON(geojson, {
-        color: 'orange'
-    }).addTo(map01)
+        pointToLayer: function (feature, latlng) {
+            return L.circle(latlng, {
+                color: 'red',
+                opacity: .6,
+            })
+        },
+    }).bringToFront().addTo(map01)
 })
