7c24e69563ce9345d1510a8954f0642ff2662530
Zhen3r <xzzxzz2010@163.com>
2021-11-26 18:21:39 -0500

    built second page

--- web/static/main.js
+++ web/static/main.js
@@ -27,11 +27,16 @@ $.getJSON('data/roads.geojson', function (geojson) {
     L.geoJSON(geojson, {
         style: (feature) => {
             let classNum = feature.properties["CLASS"];
-            // let c = chroma.scale().classes(quantileBreaks)(feature.properties["CLASS"])
-            let o = (classNum <= 3 ? 1 : .5)
+            let c = chroma.scale("viridis").classes(quantileBreaks)(feature.properties["CLASS"])
+            let w = classNum > 8 ? 0 :
+                classNum >= 5 ? 0.5 :
+                    classNum >= 3 ? 1 :
+                        1.5
+            let o = (w / 2.5) / 1
+
             return {
-                weight: 0.5,
-                color: "#000",
+                weight: .9,
+                color: c,
                 opacity: o,
             };
         }
@@ -42,9 +47,24 @@ $.getJSON('data/streetViewPoints.geojson', function (geojson) {
     L.geoJSON(geojson, {
         pointToLayer: function (feature, latlng) {
             return L.circle(latlng, {
-                color: 'red',
-                opacity: .6,
+                color: chroma.scale("viridis")(0),
+                opacity: 1,
+                weight: 1,
+                radius: 20,
             })
         },
     }).bringToFront().addTo(map01)
 })
+
+
+var map02 = L.map('map02').setView([39.9597471840457, -75.17893127441406], 14)
+basemap02.addTo(map02);
+
+var map03 = L.map('map03').setView([39.9597471840457, -75.17893127441406], 14)
+basemap02.addTo(map03);
+
+var map04 = L.map('map04').setView([39.9597471840457, -75.17893127441406], 14)
+basemap02.addTo(map04);
+
+var map05 = L.map('map05').setView([39.9597471840457, -75.17893127441406], 14)
+basemap02.addTo(map05);
