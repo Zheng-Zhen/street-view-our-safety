ff8c6d7c87d32226ec84b253a3b3a61a621f916c
Zhen3r <xzzxzz2010@163.com>
2021-12-18 13:09:24 -0500

    update web

--- web/static/main.js
+++ web/static/main.js
@@ -1,16 +1,20 @@
+// map functions
+let clearMap = function (map, basemap) {
+    map.eachLayer(function (layer) {
+        if (layer != basemap) {
+            map.removeLayer(layer);
+        }
+    });
+}
 
-// define basemap
-let basemap01 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-})
+let addDataToMap = function (map, layer, basemap) {
+    clearMap(map, basemap);
+    if (layer instanceof Array) {
+        layer.forEach(l => l.addTo(map))
+    } else layer.addTo(map)
+}
 
-let basemap02 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
-})
-
-getQuantileBreaks = function (geojson, propertyName, breakNum) {
+let getQuantileBreaks = function (geojson, propertyName, breakNum) {
     let classData = [];
     geojson.features.forEach(feature => {
         classData.push(feature.properties[propertyName]);
@@ -18,9 +22,41 @@ getQuantileBreaks = function (geojson, propertyName, breakNum) {
     return chroma.limits(classData, "q", breakNum);
 }
 
-// map01
+let generatePopup = function (name, value) {
+    return "<p><strong>" + name + "</strong>:" + value + "</p>"
+}
+
+// define basemap
+let basemap01 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+
+let basemap02 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+
+let basemap03 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+
+let basemap04 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+let basemap05 = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Carto</a>'
+})
+
+
+
+// ======= map01 ======
 var map01 = L.map('map01').setView([39.9597471840457, -75.17893127441406], 14)
-basemap02.addTo(map01);
+basemap01.addTo(map01);
+
 // Add roads
 $.getJSON('data/roads.geojson', function (geojson) {
     let quantileBreaks = getQuantileBreaks(geojson, "CLASS", 6);
@@ -56,15 +92,56 @@ $.getJSON('data/streetViewPoints.geojson', function (geojson) {
     }).bringToFront().addTo(map01)
 })
 
+// ======= map02 ======
 
-var map02 = L.map('map02').setView([39.9597471840457, -75.17893127441406], 14)
+var map02 = L.map('map02').setView([39.97, -75.17893127441406], 12)
 basemap02.addTo(map02);
 
+let pointsWithSeg;
+let renderMap02 = function (layerId) {
+    // let layerName = ['green', 'wall', 'lives', 'building', 'infrastructure', 'road', 'sidewalk',
+    //     'sky', 'green', 'transportation', 'publicservice'][layerId];
+    let layerName = layerId;
+    let q_layer = layerName + "_q";
+    if (pointsWithSeg === undefined) { console.log("loading...."); return }
+    let layer = L.geoJSON(pointsWithSeg, {
+        pointToLayer: function (feature, latlng) {
+            let q = feature.properties[q_layer];
+            return L.circle(latlng, {
+                color: chroma.scale("viridis")(q),
+                opacity: 1,
+                weight: 1,
+                radius: 20,
+            })
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                generatePopup("Segment Category", layerName) +
+                generatePopup("Percentage", (feature.properties[layerName] * 100).toFixed(2) + "%")
+            );
+        }
+    });
+    addDataToMap(map02, layer, basemap02);
+}
+
+$.getJSON('data/pointsWithSeg.geojson', function (geojson) {
+    pointsWithSeg = geojson;
+    renderMap02('green');
+})
+
+$(".map02-btn").on('click', function () {
+    let t = $(this).text();
+    renderMap02(t);
+})
+
+// ======= map03 ======
 var map03 = L.map('map03').setView([39.9597471840457, -75.17893127441406], 14)
-basemap02.addTo(map03);
+basemap03.addTo(map03);
 
+// ======= map04 ======
 var map04 = L.map('map04').setView([39.9597471840457, -75.17893127441406], 14)
-basemap02.addTo(map04);
+basemap04.addTo(map04);
 
+// ======= map05 ======
 var map05 = L.map('map05').setView([39.9597471840457, -75.17893127441406], 14)
-basemap02.addTo(map05);
+basemap05.addTo(map05);
